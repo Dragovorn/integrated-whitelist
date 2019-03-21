6020e46641a42e6a2187253c48db21d1ec68d757
Dragovorn <me@dragovorn.com>
2019-03-21 14:12:54 -0400

    Small tweaks to main class, me go to eat

--- src/main/java/com/dragovorn/integratedwhitelist/IntegratedWhitelist.java
+++ src/main/java/com/dragovorn/integratedwhitelist/IntegratedWhitelist.java
@@ -1,11 +1,21 @@
 package com.dragovorn.integratedwhitelist;
 
+import com.dragovorn.integratedwhitelist.data.IDataManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class IntegratedWhitelist extends JavaPlugin {
 
+    private static IDataManager dataManager;
+
+    private static IntegratedWhitelist instance;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+    }
+
     @Override
     public void onEnable() {
-
+        // TODO: Load configuration and perform magik
     }
 }
